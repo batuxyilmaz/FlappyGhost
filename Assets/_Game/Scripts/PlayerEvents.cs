caf49bf9f3b5b26f9525d64a5467f388962034ce
batuhanyilmaz-fildesign <batuxyilmazz@gmail.com>
2023-08-01 16:40:06 +0300

    Fixes

--- Assets/_Game/Scripts/PlayerEvents.cs
+++ Assets/_Game/Scripts/PlayerEvents.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
-using static ToonyColorsPro.ShaderGenerator.Enums;
+
 
 public class PlayerEvents : MonoBehaviour
 {
